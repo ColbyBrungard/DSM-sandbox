fb5ccc9f1dd9eec10a7b34ecb504b1043e6f6a01
Colby Brungard <colby.brungard@gmail.com>
2022-12-15 15:32:23 -0700

    gdal code to create curvature classes

--- WGScurvature_classes.bat
+++ WGScurvature_classes.bat
@@ -1,61 +1,55 @@
 @echo off
 
-REM call py3_env.bat
-set PATH=%PATH%;C:\saga-6.2.0_x64
-set SAGA_MLB=C:\saga-6.2.0_x64\tools
+
 
 REM start time 
 set startTime=%date%:%time%
 
-REM The water gathering surface input for the valley feaver interpretations requires input of plan and profile curvature classes that have been grouped into more general classes. Because I have USA-wide covariates of plan and profile curvature I decided that the easiest way to make the water gathering surface variable was to reclassify the plan and profile curvatures into concave, linear, and convex classes and then to reclassify these classes into the general water gathering classes needed. This was convienet because I already had the code to this. The two key decisions in this proceedure was 1) what neighborhood size to use and 2) what cutoff values to use. I decided on plan and profile curvature calculated over a 16-cell radius because I felt that it smoothed out much of the noise in the plan/profile values in the western USA where Valley Feaver will occur. I did observe greater fideility of plan and profile curvatures calculated over a 2 cell neighborhood to the landforms, but felt that there was just too much noise in the western 30m DEM. 
-REM I chose to use cutoff thresholds of -0.0001 and 0.0001. Values were consisdred concave if they were < = -0.0001 and convex if there were > 0.0001. Values between these two threholds were considered linear. I chose these from a visual review of the values over a hillshade and in comparision with topolines. I also tried values of 0.0005 (and -0.0005) but this only changed +- 1 cell on the edges of the class so I felt that 0.0001 was relatively robust.  
+REM The water gathering surface input for the valley feaver interpretations requires input of plan and profile curvature classes that have been grouped into more general classes. Because I have USA-wide covariates of plan and profile curvature I decided that the easiest way to make the water gathering surface variable was to reclassify the plan and profile curvatures into concave, linear, and convex classes and then to reclassify these classes into the general water gathering classes needed. This was convienet because I already had the code to this. The two key decisions in this proceedure was 1) what neighborhood size to use and 2) what cutoff values to use. I decided on plan and profile curvature calculated over a 16-cell radius because I felt that it smoothed out much of the noise in the plan/profile values in the western USA where Valley Feaver will occur. I did observe greater fideility of plan and profile curvatures calculated over a 2 cell neighborhood to the landforms, but felt that there was just too much noise in the 30m DEM of the western USA. 
+
+REM I chose to use cutoff thresholds of -0.0001 and 0.0001. Values were consisdred concave if they were < = -0.0001 and convex if there were > 0.0001. Values between these two threholds were considered linear. I chose these from a visual review of the values over a hillshade and in comparision with topolines. I also tried values of 0.0005 (and -0.0005) but this only changed +- 1 cell on the edges of the class so I felt that 0.0001 was relatively robust. 
+
+REM Clip each plan and profile curvature to the western USA as this is the only data that I need. Boundaries for the western USA were obtained from: https://catalog.data.gov/dataset/tiger-line-shapefile-2017-nation-u-s-current-state-and-equivalent-national  I then selected the states that were west of ~ 100th parallel (e.g., KS westward) and saved this as a clipping layer. Then I used this to clip the DEM. 
+
+REM REM clip to western USA. Make sure to save this as a .tif or the gdal calc will not work
+REM echo now clipping to western USA
+REM gdalwarp -t_srs EPSG:5070 -tr 30.0 30.0 -cutline tl_2017_us_state_west.gpkg -crop_to_cutline -multi -wo NUM_THREADS planc_16.tif planc_16_west.tif
+REM gdalwarp -t_srs EPSG:5070 -tr 30.0 30.0 -cutline tl_2017_us_state_west.gpkg -crop_to_cutline -multi -wo NUM_THREADS profc_16.tif profc_16_west.tif
 
 
-rem convert to saga format
-echo now converting to saga format
-gdalwarp -of SAGA planc_16.tif planc_16.sdat
-gdalwarp -of SAGA profc_16.tif profc_16.sdat
+REM echo *************************
+REM REM plan cuvature classes
+REM echo now calculating plan curvature classes 
+REM call gdal_calc -A planc_16_west.tif --outfile=plan_classes.tif --calc="3*(A<=-0.0001)+2*((A>-0.0001)*(A<=0.0001))+1*(A>0.0001)" --NoDataValue=255 --type Byte --creation-option COMPRESS=DEFLATE --creation-option BIGTIFF=YES
 
-echo *************************
-REM Cuvature classes 
+REM echo *************************
+REM rem profile curvature classes. Note that the values for convex/concave are inverse of the plan classes
+REM echo now calculating profile curvature classes
+REM call gdal_calc -A profc_16_west.tif --outfile=prof_classes.tif --calc="1*(A<=-0.0001)+2*((A>-0.0001)*(A<=0.0001))+3*(A>0.0001)" --NoDataValue=255 --type Byte --creation-option COMPRESS=DEFLATE --creation-option BIGTIFF=YES
 
-REM profile curvature classes
-echo now calculating plan curvature classes
-saga_cmd grid_tools 15 -INPUT=planc_16.sgrd -RESULT=plan_classes.sgrd -METHOD=2 -RETAB=lut_planc.txt -TOPERATOR=3
 
-echo *************************
-rem plan curvature classes
-echo now calculating profile curvature classes
-saga_cmd grid_tools 15 -INPUT=profc_16.sgrd -RESULT=prof_classes.sgrd -METHOD=2 -RETAB=lut_profc.txt -TOPERATOR=3
+REM echo *************************
+REM rem water gathering surface classes
+REM echo now calculating water gathering surface curvature classes
+REM call gdal_calc -A prof_classes.tif -B plan_classes.tif --outfile=WGSslopeshape_classes.tif --calc="1*((A==2)*(B==2))+0*((A==2)*(B==3))+2*((A==2)*(B==1))+0*((A==3)*(B==2))+0*((A==3)*(B==3))+1*((A==3)*(B==1))+2*((A==1)*(B==2))+1*((A==1)*(B==3))+3*((A==1)*(B==1))" --NoDataValue=255 --type Byte --creation-option COMPRESS=DEFLATE --creation-option BIGTIFF=YES
 
-echo *************************
-rem water gathering surface classes
-echo now calculating water gathering surface curvature classes
-saga_cmd grid_tools 20 -GRID1=prof_classes.sgrd -GRID2=plan_classes.sgrd -RESULT=WGSslopeshape_classes.sgrd -LOOKUP=lut_WGSclasses.txt
-
-echo *************************
-rem nine curvature classes 
-echo now calculating 9 slope shape classes
-saga_cmd grid_tools 20 -GRID1=prof_classes.sgrd -GRID2=plan_classes.sgrd -RESULT=slopeshape_9classes.sgrd -LOOKUP=lut_9classes.txt
+REM echo *************************
+REM rem nine curvature classes 
+REM echo now calculating 9 slope shape classes
+REM call gdal_calc -A prof_classes.tif -B plan_classes.tif --outfile=slopeshape_9classes.tif --calc="1*((A==2)*(B==2))+2*((A==2)*(B==3))+3*((A==2)*(B==1))+4*((A==3)*(B==2))+5*((A==3)*(B==3))+6*((A==3)*(B==1))+7*((A==1)*(B==2))+8*((A==1)*(B==3))+9*((A==1)*(B==1))" --NoDataValue=255 --type Byte --creation-option COMPRESS=DEFLATE --creation-option BIGTIFF=YES
 
 echo *************************
 rem 4 simplified curvature classes 
-echo now calculating 4 simplified slope shape classes
-saga_cmd grid_tools 20 -GRID1=prof_classes.sgrd -GRID2=plan_classes.sgrd -RESULT=slopeshape_4classes.sgrd -LOOKUP=lut_4classes.txt
-
-echo *************************
-rem compress
-echo now compressing
-gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 plan_classes.sdat plan_classes.tif
-gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 prof_classes.sdat prof_classes.tif
-gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 WGSslopeshape_classes.sdat WGSslopeshape_classes.tif
-gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 slopeshape_9classes.sdat slopeshape_9classes.tif
-gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 slopeshape_4classes.sdat slopeshape_4classes.tif
+echo now calculating 4 simplified slope shape classes. 
+REM this is not quite right it does not produce class4 right.... need to test on smaller area. 
+call gdal_calc -A prof_classes.tif -B plan_classes.tif --outfile=slopeshape_4classes.tif --calc="2*((A==2)*(B==2))+4*((A==2)*(B==3))+4*((A==2)*(B==1))+4*((A==3)*(B==2))+3*((A==3)*(B==3))+4*((A==3)*(B==1))+4*((A==1)*(B==2))+4*((A==1)*(B==3))+1*((A==1)*(B==1))" --NoDataValue=255 --type Byte --creation-option COMPRESS=DEFLATE --creation-option BIGTIFF=YES
 
 echo Start Time: %startTime%
 echo Finish Time: %date%:%time%
 
-REM THIS ALMOST WORKS EXCEPT THAT IT CONVERSTS because it converts everything to a 3band image with values of the RGB colors, not the right classes. This is cool though. 
+REM end ######################
+
+REM THIS ALMOST WORKS EXCEPT THAT IT converts everything to a 3band image with values of the RGB colors, not the right classes. This is cool though. 
 REM rem add colors and convert to geotif format. For some reason doing this for the 9 classes makes it a 3 band image
 REM echo now adding colors and converting
 REM gdaldem color-relief -of GTiff plan_classes.sdat color.txt plan_classes_color.tif  
@@ -68,6 +62,8 @@ REM gdaldem color-relief -of GTiff slopeshape_9classes.sdat color9.txt slopeshap
 REM rem THIS ALMOST WORKS EXCEPT THAT I CAN'T GET THE FILES TO BE TAB DELIMITED AND IF THEY ARE NOT TAB DELIMITED IT WILL FAIL. THE BEST WAY TO MAKE THESE LOOK UP TABLES IS TO GENERATE ONE IN SAGA GIS, SAVE IT AS A TEMPLATE, AND MODIFY THE VALUES AS NEEDED.
 
 REM REM -----------------------------------
+REM set PATH=%PATH%;C:\saga-6.2.0_x64
+REM set SAGA_MLB=C:\saga-6.2.0_x64\tools
 REM rem make the reclassification tables 
 REM echo now making reclassifiction look up tables. 
 
@@ -100,6 +96,7 @@ REM rem 1 = Convex - Concave
 REM rem 2 = Concave - Linear
 REM rem 1 = Concave - Convex
 REM rem 3 = Concave - Concave
+
 REM (
 rem echo Value in Grid 1 Value in Grid 2 Resulting Value
 REM echo 2.000000	2.000000	1.000000
@@ -158,4 +155,55 @@ REM REM echo 1.000000 1.000000 1.000000
 REM REM ) > "lut_4classes.txt"
 
 
+REM REM even with clipping the files were too large
+REM REM Multiply the curvature values by 1000000 then convert to integer
+REM echo now converting to integer to reduce size
+REM call gdal_calc -A planc_16_west.tif --outfile=planc_16_west_int.tif --calc="A*100000" --type Int16 --creation-option COMPRESS=DEFLATE --creation-option PREDICTOR=2 --creation-option BIGTIFF=YES
 
+REM call gdal_calc -A profc_16_west.tif --outfile=profc_16_west_int.tif --calc="A*100000" --type Int16 --creation-option COMPRESS=DEFLATE --creation-option PREDICTOR=2 --creation-option BIGTIFF=YES
+
+
+REM rem convert to saga format (happens in RAM)
+REM echo now converting to saga format
+REM gdalwarp -of SAGA planc_16_west_int.tif planc_16_west_int.sdat
+REM gdalwarp -of SAGA profc_16_west_int.tif profc_16_west_int.sdat
+
+
+REM SAGA GIS format
+REM echo *************************
+REM REM Cuvature classes 
+
+REM REM This fails because of memory issues because the raster it too large as SAGA tries to load these files into RAM. 
+REM REM profile curvature classes
+REM echo now calculating plan curvature classes
+REM saga_cmd grid_tools 15 -INPUT=planc_16_west_int.sgrd -RESULT=plan_classes.sgrd -METHOD=2 -RETAB=lut_planc_int.txt -TOPERATOR=3
+
+
+REM echo *************************
+REM rem plan curvature classes
+REM echo now calculating profile curvature classes
+REM saga_cmd grid_tools 15 -INPUT=profc_16_west_int.sgrd -RESULT=prof_classes.sgrd -METHOD=2 -RETAB=lut_profc_int.txt -TOPERATOR=3
+
+REM echo *************************
+REM rem water gathering surface classes
+REM echo now calculating water gathering surface curvature classes
+REM saga_cmd grid_tools 20 -GRID1=prof_classes.sgrd -GRID2=plan_classes.sgrd -RESULT=WGSslopeshape_classes.sgrd -LOOKUP=lut_WGSclasses.txt
+
+REM echo *************************
+REM rem nine curvature classes 
+REM echo now calculating 9 slope shape classes
+REM saga_cmd grid_tools 20 -GRID1=prof_classes.sgrd -GRID2=plan_classes.sgrd -RESULT=slopeshape_9classes.sgrd -LOOKUP=lut_9classes.txt
+
+REM echo *************************
+REM rem 4 simplified curvature classes 
+REM echo now calculating 4 simplified slope shape classes
+REM saga_cmd grid_tools 20 -GRID1=prof_classes.sgrd -GRID2=plan_classes.sgrd -RESULT=slopeshape_4classes.sgrd -LOOKUP=lut_4classes.txt
+
+REM echo *************************
+REM rem compress
+REM echo now compressing
+REM gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 plan_classes.sdat plan_classes.tif
+REM gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 prof_classes.sdat prof_classes.tif
+REM gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 WGSslopeshape_classes.sdat WGSslopeshape_classes.tif
+REM gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 slopeshape_9classes.sdat slopeshape_9classes.tif
+REM gdal_translate -ot Byte -co COMPRESS=DEFLATE -co PREDICTOR=2 slopeshape_4classes.sdat slopeshape_4classes.tif
